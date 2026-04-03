35795c52c836ca746e1ba29a2e5aadfed62b41f9
luterceiro <luciana.terceiro@gmail.com>
2026-04-03 20:30:40 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-export default defineConfig({
-  base: "/MIE26/",
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/MIE26/",
   server: {
     host: "::",
     port: 8080,
