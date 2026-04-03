6ecc82caf417691144e892e0111a1f0d42a450b4
luterceiro <luciana.terceiro@gmail.com>
2026-04-03 20:41:14 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export : defineConfig(({ mode }) => ({
   base: "/MIE26/",
   server: {
     host: "::",
